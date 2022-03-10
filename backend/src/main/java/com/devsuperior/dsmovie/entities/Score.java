ab6412d3784477b8c8075498c9e73a3b268a0e88
Jose Renato Nunes de Azevedo <jrenato.nunes@gmail.com>
2022-03-10 19:35:04 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	
+	private Double value;
+	
+	Score() {}
+
+	public void setMovie( Movie movieId) {
+		id.setMovie(movieId);
+	}
+	
+	
+	public void serUser( User userId) {
+		id.setUser(userId);
+	}
+	
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+
+}
